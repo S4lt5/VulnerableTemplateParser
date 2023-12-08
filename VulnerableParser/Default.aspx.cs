e30fe029779a44550f5bca2e8de2fafb3db84c8d
somebody <somebody>
2023-12-08 12:00:07 -0500

    Initial COmmit of vulnerable app.
    POST to /Default
    
    parameters=<%@ Register
        TagPrefix="x"
        Namespace="System.Runtime.Remoting.Services"
        Assembly="System.Runtime.Remoting, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"
    %>
    <x:RemotingService runat="server"
        Context-Response-ContentType="CW was here"
    />
    />

---
+++ VulnerableParser/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Ajax.Utilities;
+
+namespace VulnerableParser
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string parms = Request.Form["parameters"];
+                if (!String.IsNullOrEmpty(parms))
+                {
+
+                    Control c = ParseControl(parms);
+                    myPlaceholder.Controls.Add(c);
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message + "\n" + ex.StackTrace;
+                this.Response.Write(msg);
+               
+            }
+            this.Response.Write("Hi!");
+             
+            
+        }
+
+
+    }
+}
